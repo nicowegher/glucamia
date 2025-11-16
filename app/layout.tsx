20e113d11969b295fe4ae7c220a00d6437681bc2
Nicolas Wegher <nicolas.wegher@gmail.com>
2025-11-16 09:36:32 -0300

    PWA icons y nombres: mejorar instalación en Android/iOS
    - site.webmanifest: name/short_name 'Glucamia'; añadir icons 'any' 192/512
    - manifest.json: alinear con fullscreen + icons any+maskable
    - layout.tsx: appleWebApp.title 'Glucamia'
    - Resultado: icono visible en A2HS Android, nombre corto correcto, fullscreen

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   appleWebApp: {
     capable: true,
     statusBarStyle: "black-translucent",
-    title: "glucamia",
+    title: "Glucamia",
   },
   icons: {
     icon: [
